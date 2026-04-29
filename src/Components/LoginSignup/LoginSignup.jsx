30b566818470115ea3687e955beed489d3509cf3
HasherGhost <sarangpatharkar2@gmail.com>
2026-04-29 23:01:13 +0530

    made some UI changes

--- src/Components/LoginSignup/LoginSignup.jsx
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -9,26 +9,31 @@ import password_icon from '../Assets/password.png'
 export const LoginSignup = () => {
   return (
     <div className='container'>
+
         <div className='header'>
             <div className="text">Sign up</div>
             <div className="underline"></div>
         </div>
+
         <div className="inputs">
+
             <div className="input">
-                <img src={user_icon} alt="" width="30"/> 
-                <input type="text"/>
+                <img src={user_icon} alt="" width="22"/> 
+                <input type="text" placeholder='Name'/>
             </div>
 
             <div className="input">
-                <img src={email_icon} alt="" width="30"/> 
-                <input type="email"/>
+                <img src={email_icon} alt="" width="22"/> 
+                <input type="email" placeholder='E-mail'/>
             </div>
 
             <div className="input">
-                <img src={password_icon} alt="" width = "30"/> 
-                <input type="password"/>
+                <img src={password_icon} alt="" width = "22"/> 
+                <input type="password" placeholder='password'/>
             </div>
+
         </div>
+
         <div className="forgot-password">Lost Password? <span>Click Here</span></div>
         <div className="submit-container">
             <div className="submit">Sign Up</div>
